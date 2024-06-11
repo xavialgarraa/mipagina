88ddaabe89180b86ac1413d3a8b9a5d95add2991
xavialgarraa <131766832+xavialgarraa@users.noreply.github.com>
2024-06-11 19:26:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 // script.js
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById("registration-form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        registerParticipant();
+    });
+});
+
+function showRegistrationForm() {
+    document.getElementById("registration").style.display = "block";
+    document.getElementById("statistics").style.display = "block";
+}
+
+function registerParticipant() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var age = document.getElementById("age").value;
+
+    if (name && email && age) {
+        var participantsList = document.getElementById("participants-list");
+        var listItem = document.createElement("li");
+        listItem.textContent = "Nombre: " + name + ", Correo: " + email + ", Edad: " + age;
+        participantsList.appendChild(listItem);
+
+        // Limpiar el formulario
+        document.getElementById("registration-form").reset();
+    }
+}
+
 function loadYouTubeVideo() {
     var link = document.getElementById("youtube-link").value;
     var iframe = document.getElementById("youtube-frame");
@@ -11,3 +40,4 @@ function loadYouTubeVideo() {
         iframe.src = "https://www.youtube.com/embed/" + videoId;
     }
 }
+
